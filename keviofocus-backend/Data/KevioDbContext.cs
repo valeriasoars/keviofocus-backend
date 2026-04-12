4ad35377a5db790c7bf4c7aa1caa27ac4daecdb5
valeriasoars <valeriasoaressantos2@gmail.com>
2026-04-12 11:29:29 -0300

    Initial commit: Projeto configurado com SQL Server e User Secrets

---
+++ keviofocus-backend/Data/KevioDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace keviofocus_backend.Data
+{
+    public class KevioDbContext: Microsoft.EntityFrameworkCore.DbContext
+    {
+        public KevioDbContext(DbContextOptions<KevioDbContext> options): base(options) { }
+
+    }
+}
